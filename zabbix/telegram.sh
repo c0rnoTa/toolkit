acfb63926dd16f23c2afb29c14abdf8cc31b778e
Anton Zakharov <c0rnota@gmail.com>
2020-10-28 13:35:58 +0300

    Save telegram notification script

---
+++ zabbix/telegram.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+[[ $# -ne 4 ]] echo 'Error! You must to define four params' && exit 1
+
+TOKEN="$1"
+CHAT_ID="$2"
+SUBJECT="$3"
+MESSAGE="$4"
+
+curl -s  --header 'Content-Type: application/json' --request 'POST' --data "{\"chat_id\":\"${CHAT_ID}\",\"text\":\"${SUBJECT}\n${MESSAGE}\"}" "https://api.telegram.org/bot${TOKEN}/sendMessage" | grep -q '"ok":false,'
+
+[[ $? -eq 0 ]] exit 1
